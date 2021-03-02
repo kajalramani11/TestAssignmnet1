5f326f51aff0640eb4e9528db5ca71c9c580692b
kajal.Ramani <kajal.ramani@thegatewaycorp.com>
2021-03-02 18:57:38 +0530

    xunit updated

--- XUnitTestProject1/DataInitializer.cs
+++ XUnitTestProject1/DataInitializer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
+using Testing1.Controllers;
 
 namespace XUnitTestProject1
 {
@@ -12,14 +13,61 @@ namespace XUnitTestProject1
         [Fact]
         public void CreateUser()
         {
-            //
-            //
-            //
+            //Act
+            var id = 1;
+            var fname = "Kajal";
+            var lname = "Ramani";
+            var phone = 8866554433;
+            var expectedResult = "Kajal Ramani 8866554433";
+
+            //Arrange
+            var result = PassenegrsController.Create(id, fname, lname, phone);
+
+            //Assert
+            Assert.Equal(expectedResult, result,1);
+        }
+
+        public void Details()
+        {
+            //Act
+            var id = 1;
+            var expectedResult = "Kajal Ramani 8866554433";
+
+            //Arrange
+            var result = PassenegrsController.Details(id);
+
+            //Assert
+            Assert.Equal(expectedResult, result, 1);
+        }
+
+        public void Update()
+        {
+            //Act
+            var id = 1;
+            var fname = "K";
+            var lname = "R";
+            var phone = 8866554433;
+            var expectedResult = "K R 8866554433";
+
+            //Arrange
+            var result = PassenegrsController.Edit(id);
+
+            //Assert
+            Assert.Equal(expectedResult, result, 1);
         }
 
         public void Delete()
         {
+            //Act
+            var id = 1;
+            var phone = 8866554433;
+            var expectedResult = "";
 
+            //Arrange
+            var result = PassenegrsController.Delete(id);
+
+            //Assert
+            Assert.Equal(expectedResult, result, 1);
         }
     }
 }
